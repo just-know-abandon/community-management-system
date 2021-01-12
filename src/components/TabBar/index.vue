625e97a102be2c3d9a39ec1ad634c987d4bb9b66
lhj1101 <751609117@qq.com>
2021-01-13 02:07:39 +0800

    feat：tarbar完成

---
+++ src/components/TabBar/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tabBar">
+    <router-link to="/index" class="tabBar-item">
+      <i class="iconfont icon-shouye"></i>
+      <span>首页</span>
+    </router-link>
+    <router-link to="/market" class="tabBar-item">
+      <i class="iconfont icon-shouye"></i>
+      <span>市场</span>
+    </router-link>
+    <router-link to="/chatRoom" class="tabBar-item">
+      <i class="iconfont icon-shouye"></i>
+      <span>聊天室</span>
+    </router-link>
+    <router-link to="/share" class="tabBar-item">
+      <i class="iconfont icon-shouye"></i>
+      <span>分享</span>
+    </router-link>
+    <router-link to="/personalCenter" class="tabBar-item">
+      <i class="iconfont icon-shouye"></i>
+      <span>个人</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TarBar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../assets/less/index.less';
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+// FastClick
+import FastClick from 'fastclick'
+// 移动端适配
+import 'amfe-flexible'
+// 全局样式表
+import './assets/css/global.css'
+// 导入iconfont
+import './assets/fonts/iconfont.css'
+
+// FastClick绑定body
+FastClick.attach(document.body)
+
+// 全局注册axios
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
